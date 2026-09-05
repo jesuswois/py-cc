680c7d90eec47e4fe131b90f853127949b02c327
Jesús Domínguez Ramírez <230028@utxicotepec.edu.mx>
2026-09-05 10:36:41 -0600

    Blue Sky Exercise & Refactoring Chapter init

--- proyects/alien_invasion/a_ship_that_fires_bullets/blue_sky.py
+++ proyects/alien_invasion/a_ship_that_fires_bullets/blue_sky.py
@@ -1,2 +1,19 @@
 # 12-1. Blue Sky: Make a Pygame window with a blue background.
+import sys
+import pygame
 
+def run_game():
+    pygame.init()
+
+    screen = pygame.display.set_mode((1200,600))
+    pygame.display.set_caption("Blue Sky")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        screen.fill((0, 0, 150))
+        pygame.display.flip()
+
+run_game()
