bd608fc1a50a290ea2d5db9f37103c3385984efa
Jesús Domínguez Ramírez <230028@utxicotepec.edu.mx>
2026-09-09 16:03:14 -0600

    Piloting The Ship - Rocket Exercise
    settings.py finished

---
+++ proyects/alien_invasion/piloting_the_ship/exercises/rocket/settings.py
@@ -0,0 +1,6 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = (125,125,125)
+        self.rocket_movement_multiplier = 1.5
