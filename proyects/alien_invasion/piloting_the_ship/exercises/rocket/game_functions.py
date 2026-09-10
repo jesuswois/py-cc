e7bad2edc693b36fb0dbe7e152696c3d19c9a3f8
Jesús Domínguez Ramírez <230028@utxicotepec.edu.mx>
2026-09-10 16:50:25 -0600

    Piloting The Ship - Rocket Exercise
    Fixed Movement in the Y Axis

--- proyects/alien_invasion/piloting_the_ship/exercises/rocket/game_functions.py
+++ proyects/alien_invasion/piloting_the_ship/exercises/rocket/game_functions.py
@@ -13,18 +13,22 @@ def check_events(rocket):
 def check_keydown_events(event, rocket):
     if event.key == pygame.K_UP:
         rocket.moving_up = True
+        print("up")
     elif event.key == pygame.K_RIGHT:
         rocket.moving_right = True
+        print("right")
     elif event.key == pygame.K_LEFT:
         rocket.moving_left = True
+        print("left")
     elif event.key == pygame.K_DOWN:
+        print("down")
         rocket.moving_down = True
 
 def check_keyup_events(event, rocket):
     if event.key == pygame.K_UP:
         rocket.moving_up = False
     elif event.key == pygame.K_RIGHT:
-        rocket.moving_rigth = False
+        rocket.moving_right = False
     elif event.key == pygame.K_DOWN:
         rocket.moving_down = False
     elif event.key == pygame.K_LEFT:
